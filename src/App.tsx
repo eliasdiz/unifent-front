a35ccb20502718d7646b2e8ec7a1472da5ae9f4b
elias <eliasj.diz@gmail.com>
2025-02-11 22:08:30 -0500

    inicio del proyecto e instalacion de materialui + tailwind

---
+++ src/App.tsx
@@ -0,0 +1,18 @@
+import { Button } from "@mui/material"
+
+
+const App = () => {
+	return (
+		<div className="h-[100vh] border border-red-500">
+			
+			<Button
+				variant="contained"
+				color="info"
+			>
+				sdfsdfsdf
+			</Button>
+		</div>
+	)
+}
+
+export default App
